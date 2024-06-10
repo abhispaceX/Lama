790b7423771b934c0d150922765fb5b81288054e
abhispaceX <abhiramgaddam13@gmail.com>
2024-06-10 18:38:11 +0530

    nhaa

--- src/homepage/index.js
+++ src/homepage/index.js
@@ -15,11 +15,11 @@ const CreateProject = () => {
                     <img src={image} alt="Project" />
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
                     <Link className='link' to="/projectcreation">
-                        <button className="create-element">
-                            New Project
-                        </button>
+                        <p className="create-element">
+                            Create New Project
+                        </p>
                     </Link>
-                    <p>Create</p>
+                    
                 </div>
             </div>
         </>
