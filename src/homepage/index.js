6155945bcf4b4db11080f9b4b52349a15ae8a8f8
abhispaceX <abhiramgaddam13@gmail.com>
2024-06-10 18:30:54 +0530

    changed button

--- src/homepage/index.js
+++ src/homepage/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './index.css'; // Import the CSS file for styling
 import image from "../assets/image.png";
-import plus from "../assets/plus.png";
+
 import Header from '../navbar';
 
 const CreateProject = () => {
@@ -16,8 +16,7 @@ const CreateProject = () => {
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
                     <Link className='link' to="/projectcreation">
                         <button className="create-element">
-                            <img className='plusbuttonn' src={plus} alt="plus" />
-                            Create New Project
+                            New Project
                         </button>
                     </Link>
                 </div>
