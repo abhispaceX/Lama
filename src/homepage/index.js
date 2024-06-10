1acbdc5f4044797ff40a960f36faff43e502bca3
abhispaceX <abhiramgaddam13@gmail.com>
2024-06-10 18:58:33 +0530

    button fixed

--- src/homepage/index.js
+++ src/homepage/index.js
@@ -15,9 +15,12 @@ const CreateProject = () => {
                     <img src={image} alt="Project" />
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
                     <Link className='link' to="/projectcreation">
-                        <p className="create-element">
+                   
+                        <button  className='abhi-create' >
                             Create New Project
-                        </p>
+                        </button>
+                        
+                        
                     </Link>
                     
                 </div>
